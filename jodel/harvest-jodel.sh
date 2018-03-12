ab7f22c2514ec9bb1bb1aed681ded99aecd5ab97
jrg <jrg@statsbiblioteket.dk>
2018-03-12 15:45:14 +0100

    Added todo

--- jodel/harvest-jodel.sh
+++ jodel/harvest-jodel.sh
@@ -4,3 +4,5 @@ source accounts/Aarhus-account-data.sh
 
 ./harvest-jodel.py "$latitude" "$longitude" "$city" "$access_token" "$expiration_date" "$refresh_token" "$distinct_id" "$device_uid"
 
+# TODO add more cities, by repeating the two lines above for each city
+
