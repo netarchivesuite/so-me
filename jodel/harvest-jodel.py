5b9f648866931169b6bd3cb0e9150db34a278b6b
jrg <jrg@statsbiblioteket.dk>
2018-04-06 14:39:49 +0200

    Now saving refreshed data to file

--- jodel/harvest-jodel.py
+++ jodel/harvest-jodel.py
@@ -187,6 +187,17 @@ if expiration_date <= unix_time_now:
     refresh_result = account.refresh_access_token()
     access_token = refresh_result[1]['access_token']
     expiration_date = refresh_result[1]['expiration_date']
+    # Generate and print bash-sourcable variable-definitions to file
+    with open("accounts/" + city + "-account-data.sh", "w") as f:
+        print >>f, "latitude='" + str(lat) + "'"
+        print >>f, "longitude='" + str(lng) + "'"
+        print >>f, "city='" + city + "'"
+        print >>f, "access_token='" + account_data["access_token"] + "'"
+        print >>f, "expiration_date='" + str(account_data["expiration_date"])
+            + "'"
+        print >>f, "refresh_token='" + account_data["refresh_token"] + "'"
+        print >>f, "distinct_id='" + account_data["distinct_id"] + "'"
+        print >>f, "device_uid='" + account_data["device_uid"] + "'"
 
 # Dictionary with post_id as key and timestamp as value.
 # Alive posts are posts that had not been deleted(via downvotes/deletion) or
