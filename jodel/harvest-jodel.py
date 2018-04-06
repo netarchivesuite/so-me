31732fed6ffb3a496d714549864b0c55330ceb28
jrg <jrg@statsbiblioteket.dk>
2018-04-06 14:40:49 +0200

    Minor fix

--- jodel/harvest-jodel.py
+++ jodel/harvest-jodel.py
@@ -193,8 +193,7 @@ if expiration_date <= unix_time_now:
         print >>f, "longitude='" + str(lng) + "'"
         print >>f, "city='" + city + "'"
         print >>f, "access_token='" + account_data["access_token"] + "'"
-        print >>f, "expiration_date='" + str(account_data["expiration_date"])
-            + "'"
+        print >>f, "expiration_date='" + str(account_data["expiration_date"]) + "'"
         print >>f, "refresh_token='" + account_data["refresh_token"] + "'"
         print >>f, "distinct_id='" + account_data["distinct_id"] + "'"
         print >>f, "device_uid='" + account_data["device_uid"] + "'"
