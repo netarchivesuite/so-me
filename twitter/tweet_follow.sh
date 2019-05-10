0075a78e5f84b47b9a8e79d807f5265aa8fa9f8c
Toke Eskildsen <toes@kb.dk>
2019-05-10 16:34:06 +0200

    Chunk lookups with delays in between to avoid hammer-banning

--- twitter/tweet_follow.sh
+++ twitter/tweet_follow.sh
@@ -24,9 +24,11 @@ fi
 : ${HANDLES:="$1"}
 : ${OUTBASE:="twitter_users"}
 : ${OUTDESIGNATION:="$2"}
-: ${RUNTIME:="3600"} # Seconds
+: ${RUNTIME:="3780"} # Seconds
 : ${HARVEST:="true"} # Harvest linked resources
 : ${TWARC:="$(which twarc)"}
+: ${LOOKUP_CHUNK_SIZE:="200"}
+: ${CALM_TIME:="7"}
 source tweet_common.sh
 popd > /dev/null
 
@@ -64,14 +66,43 @@ check_parameters() {
 # FUNCTIONS
 ################################################################################
 
+resolve_chunked() {
+    local HANDLES="$1"
+    $TWARC users "$HANDLES"
+}
+
+
+chunk_profiles() {
+    local ALL_HANDLES=$(tr ',' '\n' <<< "$1")
+    COUNTER=0
+    HANDLES=""
+    while read HANDLE; do
+        if [[ "." != ".$HANDLES" ]]; then
+            HANDLES="${HANDLES},"
+        fi
+        HANDLES="${HANDLES}${HANDLE}"
+        COUNTER=$(( COUNTER+1 ))
+        if [[ "$COUNTER" -eq "$LOOKUP_CHUNK_SIZE" ]]; then
+            resolve_chunked "$HANDLES"
+            HANDLES=""
+            COUNTER=0
+            sleep $CALM_TIME
+        fi
+    done <<< "$ALL_HANDLES"
+    if [[ "." != ".$HANDLES" ]]; then
+        resolve_chunked "$HANDLES"
+    fi
+}
+
 # Following Twitter profiles is done by following user-IDs, not handles.
 # The handles needs to be resolved to IDs first.
 #
-# Input: HANDLES
-# Output: IDS
+# Input: HANDLES (comma separated)
+# Output: IDS (comma separated)
 resolve_user_profiles() {
     echo " - Resolving user profiles from handles to $OUT_PROFILES"
-    $TWARC users "$HANDLES" | gzip > $OUT_PROFILES
+#    $TWARC users "$HANDLES" | gzip > $OUT_PROFILES
+    chunk_profiles "$HANDLES" | gzip > $OUT_PROFILES
     IDS=$(zcat $OUT_PROFILES | jq -r .id_str | tr '\n' ',' | sed 's/,$//')
     if [[ "." == "$IDS" ]]; then
         >&2 echo "Error: Inable to extract any IDs from $OUT_PROFILES"
@@ -90,5 +121,6 @@ follow_users() {
 
 check_parameters "$@"
 resolve_user_profiles
+sleep $CALM_TIME
 follow_users
 post_process_harvested_tweets "$OUT_TWEETS"
