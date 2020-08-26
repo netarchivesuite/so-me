c281d7e5c77afc438070b6a4282360353cebf2e0
Toke Eskildsen <toes@kb.dk>
2020-08-26 15:14:15 +0200

    Minor hacking

--- twitter/tweet_follow.sh
+++ twitter/tweet_follow.sh
@@ -12,7 +12,6 @@
 #
 
 # TODO: How do we represent friends & followers?
-# TODO: The cache stores the returned screen_name rather than the searched
 
 ###############################################################################
 # CONFIG
@@ -25,7 +24,6 @@ fi
 : ${HANDLES:="$1"}
 : ${OUTBASE:="twitter_users"}
 : ${OUTDESIGNATION:="$2"}
-: ${OUTDESIGNATION:="noname"}
 : ${RUNTIME:="3780"} # Seconds
 : ${HARVEST:="true"} # Harvest linked resources
 : ${TWARC:="$(which twarc)"}
@@ -101,7 +99,7 @@ chunk_profiles() {
 #
 # Input: HANDLES (comma separated)
 # Output: IDS (comma separated)
-resolve_user_profiles_old() {
+resolve_user_profiles() {
     echo " - Resolving user profiles from handles to $OUT_PROFILES"
 #    $TWARC users "$HANDLES" | gzip > $OUT_PROFILES
     chunk_profiles "$HANDLES" | gzip > $OUT_PROFILES
@@ -109,55 +107,6 @@ resolve_user_profiles_old() {
     if [[ "." == "$IDS" ]]; then
         >&2 echo "Error: Inable to extract any IDs from $OUT_PROFILES"
     fi
-}
-
-resolve_user_profiles_and_update_cache() {
-    local MISSING="$1"
-    chunk_profiles "$MISSING" | gzip > $OUT_PROFILES
-
-    # Store resolved IDs
-    local EPOCH=$(date +%s)
-    zcat $OUT_PROFILES | jq -r "[.screen_name,.id_str,\"$EPOCH\"] | @csv" | tr -d '"' | tr ',' ' ' >> twitter_handles.dat
-
-    # Return resolved IDs
-    zcat $OUT_PROFILES | jq -r .id_str
-}
-
-# Existing: 
-
-handles_to_ids() {
-    if [[ ! -s twitter_handles.dat ]]; then
-        echo "# handle id epoch" >> twitter_handles.dat
-    fi
-
-    # Check cache
-    UNRESOLVED=""
-    while read -r HANDLE; do
-        # handle id epoch
-        ID=$(grep -i "$HANDLE " twitter_handles.dat | cut -d\  -f2)
-        if [[ -z $ID ]]; then
-            if [[ "." != ".$UNRESOLVED" ]]; then
-                UNRESOLVED="${UNRESOLVED},"
-            fi
-            UNRESOLVED="${UNRESOLVED}${HANDLE}"
-        else
-            echo $ID
-        fi
-    done <<< $(tr ',' '\n' <<< "$HANDLES")
-
-    
-    # All handles resolved
-    if [[ "." == ".$UNRESOLVED" ]]; then
-        exit
-    fi
-
-    echo "$(date +'%Y-%m-%d %H:%M'): Resolving missing handles $UNRESOLVED" >> twitter_handles.log
-    resolve_user_profiles_and_update_cache "$UNRESOLVED"
-    sleep $CALM_TIME
-}
-
-resolve_ids() {
-    IDS=$(handles_to_ids | tr '\n' ',' | tr '\n' ',' | sed 's/,$//')
 }
 
 follow_users() {
@@ -171,7 +120,7 @@ follow_users() {
 ###############################################################################
 
 check_parameters "$@"
-# resolve_user_profiles
-resolve_ids
+resolve_user_profiles
+sleep $CALM_TIME
 follow_users
 post_process_harvested_tweets "$OUT_TWEETS"
